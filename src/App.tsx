3133c09046752f0c24a98a8242e0675f8552cdf9
Josh Wheeler <jltwheeler@gmail.com>
2022-01-16 10:09:19 +1000

    feat(landing page): add changelog styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,16 @@ import { ReactComponent } from '../CHANGELOG.md';
 function App() {
   return (
     <div className="h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
-      <header className="text-center">
-        <h1 className="text-6xl">Hello world</h1>
-        <ReactComponent />
+      <header className="flex items-center justify-center">
+        <h1 className="leading-normal font-bold text-slate-900 text-4xl py-1.5">
+          Auto ChangeLog
+        </h1>
       </header>
+      <div className="flex items-center justify-center py-8 bg-slate-100">
+        <div className="max-w-prose">
+          <ReactComponent />
+        </div>
+      </div>
     </div>
   );
 }
